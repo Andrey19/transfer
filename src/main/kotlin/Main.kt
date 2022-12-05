0cb3bb6e48b0c1a0e2d01b739228fd56ca738e8a
Andrei Razborov <andreyrd94@gmail.com>
2022-12-05 14:41:50 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val amount: Int = 35
+    val commission: Double = 0.0075
+
+    val result = amount * commission
+    println("Комиссия за перевод  составит : " + result + " рублей ")
+}
